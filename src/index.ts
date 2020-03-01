df2e13c656d9f3a49d453cf2e6677493712587a5
Fabio Ferreira <fabiomf@protonmail.com>
2020-03-01 15:58:32 -0400

    Add all methods to CORS policy

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
